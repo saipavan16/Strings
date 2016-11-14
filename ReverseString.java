35507cebc133405d160144c6cbb8b93b1afffe3f
saipavan16 <t.saipavan16@gmail.com>
2016-11-13 21:15:41 -0800

    Create ReverseString.java

---
+++ ReverseString.java
@@ -0,0 +1,32 @@
+//Reverse the string using the Array without affecting special charaters.
+class ReverseString{
+
+	public static void main(String [] args){
+		System.out.println(revString("a,b$c"));
+	}
+	
+	public static String revString(String input){
+		
+		char [] inputarr = input.toCharArray();
+		int l = 0;
+		int r = inputarr.length - 1;
+		while(l<r){
+			if(!Character.isAlphabetic(inputarr[l])){
+				l++;
+			}
+			else if (!Character.isAlphabetic(inputarr[r])){
+			r--;
+			}
+		else{
+			
+			char tempchar = inputarr[l];
+			inputarr[l] = inputarr[r];
+			inputarr[r] = tempchar;
+			l++;
+			r--;
+		}
+	}
+		return new String(inputarr);
+	}
+}
+
